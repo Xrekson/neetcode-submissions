fb15ca0085afe8aa76fcf43f37d959c699b2b031
Indraneel Brahma <76116355+Xrekson@users.noreply.github.com>
2026-06-15 23:30:35 +0530

    Add: dynamicArray - submission-5

---
+++ Data Structures & Algorithms/dynamicArray/submission-5.cpp
@@ -0,0 +1,50 @@
+class DynamicArray {
+    private:
+    int capa;
+    int len;
+    int *mr;
+
+public:
+
+    DynamicArray(int capacity) {
+        mr = new int[capacity];
+        capa = capacity;
+        len = 0;
+    };
+
+    int get(int i) {
+            return mr[i];
+    };
+
+    void set(int i, int n) {
+        mr[i] = n;
+    };
+
+    void pushback(int n) {
+        if(capa == len) resize();
+            mr[len++] = n;
+    };
+
+    int popback() {
+        len--;
+        return mr[len];
+    };
+
+    void resize() {
+            int *temp = mr;
+            capa *=2;
+            mr = new int[capa];
+            for(int i=0;i<len;i++){
+                mr[i] = temp[i];
+            };
+            delete[] temp;
+    }
+
+    int getSize() {
+        return  len;
+    }
+
+    int getCapacity() {
+        return  capa;
+    }
+};
